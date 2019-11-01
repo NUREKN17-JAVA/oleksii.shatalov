b9df48b1c5e658ccb530e76f79138fc83ac636f7
Oleksii <oleksii.shatalov@nure.ua>
2019-11-02 01:05:55 +0200

    SQL query implementation for creating the tuple

--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/db/HsqldbUserDao.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/db/HsqldbUserDao.java
@@ -1,11 +1,16 @@
 package ua.nure.cs.shatalov.usermanagement.domain.db;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import ua.nure.cs.shatalov.usermanagement.domain.User;
 
 public class HsqldbUserDao implements UserDao {
 
+	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?";
 	private ConnectionFactory connectionFactory;
 	
 	public HsqldbUserDao(ConnectionFactory connectionFactory) {
@@ -15,7 +20,23 @@ public class HsqldbUserDao implements UserDao {
 	@Override
 	public User create(User user) throws DatabaseException {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2, user.getLastName());
+			statement.setDate(3, new Date(user.getDateOfBirth().getTime()));
+			int n = statement.executeUpdate();
+			if (n != 1) {
+				throw new DatabaseException("Number of the inserted rows: " + n);
+			}
+			return null;
+		} catch (DatabaseException e) {
+			throw e;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new DatabaseException(e);
+		}
 	}
 
 	@Override
