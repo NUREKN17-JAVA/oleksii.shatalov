0cffea106f1d9dc47c4a5ae46566aec64064e03e
Oleksii <oleksii.shatalov@nure.ua>
2019-11-04 00:55:56 +0200

    findAll() function implementation

--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/db/HsqldbUserDao.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/db/HsqldbUserDao.java
@@ -6,12 +6,15 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
+import java.util.LinkedList;
 
 import ua.nure.cs.shatalov.usermanagement.domain.User;
 
 public class HsqldbUserDao implements UserDao {
 
+	private static final String SELECT_ALL_QUERY = "SELECT id, firstname, lastname, dateofbirth FROM users";
 	private static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
 	private ConnectionFactory connectionFactory;
 	
@@ -71,7 +74,29 @@ public class HsqldbUserDao implements UserDao {
 	@Override
 	public Collection findAll() throws DatabaseException {
 		// TODO Auto-generated method stub
-		return null;
+		Collection result = new LinkedList();
+		
+		try {
+			Connection connection = connectionFactory.createConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(SELECT_ALL_QUERY);
+			while (resultSet.next()) {
+				User user = new User();
+				user.setId(new Long(resultSet.getLong(1)));
+				user.setFirstName(resultSet.getString(2));
+				user.setLastName(resultSet.getString(3));
+				user.setDateOfBirth(resultSet.getDate(4));
+				result.add(user);
+			}
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			throw e;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new DatabaseException(e);
+		}
+		
+		return result;
 	}
 
 }
