f5e3ec266e776a155a2d36f4213e427508cd366c
Oleksii <oleksii.shatalov@nure.ua>
2019-11-21 16:07:15 +0200

    The main window creation

---
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/gui/BrowsePanel.java
@@ -0,0 +1,11 @@
+package ua.nure.cs.shatalov.usermanagement.domain.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+
+	public BrowsePanel(MainFrame mainFrame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/gui/MainFrame.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/gui/MainFrame.java
@@ -1,11 +1,56 @@
 package ua.nure.cs.shatalov.usermanagement.domain.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.omg.CORBA.INITIALIZE;
 
 public class MainFrame extends JFrame {
 
+	private static final int FRAME_HEIGHT = 600;
+	private static final int FRAME_WIDTH = 800;
+	private JPanel contentPanel;
+	private JPanel browsePanel;
+
+	public MainFrame() {
+		super();
+		initialize();
+	}
+	
+	private void initialize() {
+		// TODO Auto-generated method stub
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		this.setTitle("User management");
+		this.setContentPane(getJContentPanel());
+	}
+
+	private JPanel getJContentPanel() {
+		// TODO Auto-generated method stub
+		if (contentPanel == null) {
+			contentPanel = new JPanel();
+			contentPanel.setLayout(new BorderLayout());
+			contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+		}
+		return contentPanel;
+	}
+
+	private JPanel getBrowsePanel() {
+		// TODO Auto-generated method stub
+		if (browsePanel == null) {
+			browsePanel = new BrowsePanel(this);
+		}
+		return browsePanel;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
 
 	}
 
