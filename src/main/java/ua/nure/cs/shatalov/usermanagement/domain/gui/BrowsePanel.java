4e9aaff5e8e6212459a9ad9ccdc6a90f80bab44c
Oleksii <oleksii.shatalov@nure.ua>
2019-11-21 22:45:45 +0200

    View panel implementation

--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/gui/BrowsePanel.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/gui/BrowsePanel.java
@@ -1,11 +1,117 @@
 package ua.nure.cs.shatalov.usermanagement.domain.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
-public class BrowsePanel extends JPanel {
+public class BrowsePanel extends JPanel implements ActionListener {
 
-	public BrowsePanel(MainFrame mainFrame) {
+	private MainFrame parent;
+	private JPanel buttonPanel;
+	private JButton addButton;
+	private JButton detailsButton;
+	private JButton deleteButton;
+	private JButton editButton;
+	private JScrollPane tablePanel;
+	private JTable userTable;
+
+	public BrowsePanel(MainFrame frame) {
 		// TODO Auto-generated constructor stub
+		parent = frame;
+		initialize();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		this.setName("browsePanel");
+		this.setLayout(new BorderLayout());
+		this.add(getTablePanel(), BorderLayout.CENTER);
+		this.add(getButtonsPanel(), BorderLayout.SOUTH);
+	}
+
+	private JPanel getButtonsPanel() {
+		// TODO Auto-generated method stub
+		if (buttonPanel == null) {
+			buttonPanel = new JPanel();
+			buttonPanel.add(getAddButton(), null);
+			buttonPanel.add(getEditButton(), null);
+			buttonPanel.add(getDeleteButton(), null);
+			buttonPanel.add(getDetailsButton(), null);
+		}
+		return buttonPanel;
+	}
+
+	private JButton getDetailsButton() {
+		// TODO Auto-generated method stub
+		if (detailsButton == null) {
+			detailsButton = new JButton();
+			detailsButton.setText("Details");
+			detailsButton.setName("detailsButton");
+			detailsButton.addActionListener(this);
+		}
+		return detailsButton;
+	}
+
+	private JButton getDeleteButton() {
+		// TODO Auto-generated method stub
+		if (deleteButton == null) {
+			deleteButton = new JButton();
+			deleteButton.setText("Delete");
+			deleteButton.setName("deleteButton");
+			deleteButton.addActionListener(this);
+		}
+		return deleteButton;
+	}
+
+	private JButton getEditButton() {
+		// TODO Auto-generated method stub
+		if (editButton == null) {
+			editButton = new JButton();
+			editButton.setText("Edit");
+			editButton.setName("editButton");
+			editButton.addActionListener(this);
+		}
+		return editButton;
+	}
+
+	private JButton getAddButton() {
+		// TODO Auto-generated method stub
+		if (addButton == null) {
+			addButton = new JButton();
+			addButton.setText("Add");
+			addButton.setName("addButton");
+			addButton.addActionListener(this);
+		}
+		return addButton;
+	}
+
+	private JScrollPane getTablePanel() {
+		// TODO Auto-generated method stub
+		if(tablePanel == null) {
+			tablePanel = new JScrollPane(getUserTable());
+		}
+		return tablePanel;
+	}
+
+	private JTable getUserTable() {
+		// TODO Auto-generated method stub
+		if (userTable == null) {
+			userTable = new JTable();
+			userTable.setName("userTable");
+		}
+		return userTable;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
