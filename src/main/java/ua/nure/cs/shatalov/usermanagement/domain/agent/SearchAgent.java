9f8bd91e35b5ffe8ca419e377d5999888998e081
Oleksii <oleksii.shatalov@nure.ua>
2019-12-15 11:17:02 +0200

    Message processing and registration in the 'Yellow pages' service of the jade platform implementation

--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/agent/SearchAgent.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/agent/SearchAgent.java
@@ -4,6 +4,10 @@ import java.util.Collection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import ua.nure.cs.shatalov.usermanagement.domain.db.DaoFactory;
 import ua.nure.cs.shatalov.usermanagement.domain.db.DatabaseException;
 
@@ -14,12 +18,31 @@ public class SearchAgent extends Agent {
 		// TODO Auto-generated method stub
 		super.setup();
 		System.out.println(getAID().getName() + " started");
+		
+		DFAgentDescription description = new DFAgentDescription();
+		description.setName(getAID());
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setName("JADE-searching");
+		serviceDescription.setType("searching");
+		description.addServices(serviceDescription);
+		try {
+			DFService.register(this, description);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+		
+		addBehaviour(new RequestServer());
 	}
 
 	@Override
 	protected void takeDown() {
 		// TODO Auto-generated method stub
 		System.out.println(getAID().getName() + " terminated");
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
 		super.takeDown();
 	}
 
