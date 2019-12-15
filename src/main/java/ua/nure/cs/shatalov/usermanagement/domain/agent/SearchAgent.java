14bc97032be78187a15b08608625bfd33a5a82e1
Oleksii <oleksii.shatalov@nure.ua>
2019-12-15 11:28:39 +0200

    Searching agents getting list

--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/agent/SearchAgent.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/agent/SearchAgent.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -13,6 +14,8 @@ import ua.nure.cs.shatalov.usermanagement.domain.db.DatabaseException;
 
 public class SearchAgent extends Agent {
 
+	private AID[] aids;
+	
 	@Override
 	protected void setup() {
 		// TODO Auto-generated method stub
@@ -31,6 +34,27 @@ public class SearchAgent extends Agent {
 			e.printStackTrace();
 		}
 		
+		addBehaviour(new TickerBehaviour(this, 6000) {
+			@Override
+			protected void onTick() {
+				// TODO Auto-generated method stub
+				DFAgentDescription agentDescription = new DFAgentDescription();
+				ServiceDescription serviceDescription = new ServiceDescription();
+				serviceDescription.setType("searching");
+				agentDescription.addServices(serviceDescription);
+				try {
+					DFAgentDescription[] descriptions = 
+							DFService.search(myAgent, agentDescription);
+					aids = new AID[descriptions.length];
+					for (int i = 0; i < descriptions.length; i++) {
+						DFAgentDescription d = descriptions[i];
+						aids[i] = d.getName();
+					}
+				} catch (FIPAException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		addBehaviour(new RequestServer());
 	}
 
@@ -52,7 +76,7 @@ public class SearchAgent extends Agent {
 			if (users.size() > 0) {
 				showUsers(users);
 			} else {
-				addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName));
+				addBehaviour(new SearchRequestBehaviour(aids, firstName, lastName));
 			}
 		} catch (DatabaseException e) {
 			throw new SearchException(e);
