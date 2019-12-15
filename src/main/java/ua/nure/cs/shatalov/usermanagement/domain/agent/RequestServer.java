287fe46ed891445b6022e3d79b0c31797f0fc595
Oleksii <oleksii.shatalov@nure.ua>
2019-12-15 11:08:05 +0200

    Method for response messages decomposition implementation

--- src/main/java/ua/nure/cs/shatalov/usermanagement/domain/agent/RequestServer.java
+++ src/main/java/ua/nure/cs/shatalov/usermanagement/domain/agent/RequestServer.java
@@ -2,6 +2,7 @@ package ua.nure.cs.shatalov.usermanagement.domain.agent;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 import org.hsqldb.lib.Iterator;
@@ -32,7 +33,20 @@ public class RequestServer extends CyclicBehaviour {
 
 	private Collection parseMessage(ACLMessage message) {
 		// TODO Auto-generated method stub
-		return null;
+		Collection users = new LinkedList();
+		String content = message.getContent();
+		if (content != null) {
+			StringTokenizer tokenizer1 = new StringTokenizer(content, ";");
+			while(tokenizer1.hasMoreTokens()) {
+				String userInfo = tokenizer1.nextToken();
+				StringTokenizer tokenizer2 = new StringTokenizer(userInfo, ",");
+				String id = tokenizer2.nextToken();
+				String firstName = tokenizer2.nextToken();
+				String lastName = tokenizer2.nextToken();
+				users.add(new User(new Long(id), firstName, lastName, null));
+			}
+		}
+		return users;
 	}
 
 	private ACLMessage createReply(ACLMessage message) {
